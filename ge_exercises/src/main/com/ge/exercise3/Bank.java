94c21c662817b46aae17d569373178f6ff0f0998
Scott Friery <frierys@gmail.com>
2018-03-24 15:33:44 -0400

    Added current holding and account protections

--- ge_exercises/src/main/com/ge/exercise3/Bank.java
+++ ge_exercises/src/main/com/ge/exercise3/Bank.java
@@ -9,6 +9,8 @@ import java.util.Map;
 public class Bank {
 
     private static final Logger logger = LogManager.getLogger(Bank.class);
+    private static final float OVERDRAFT_LIMIT = -100;
+    
     private Map<String, Account> accountMap;
 
     public Bank() {
@@ -28,10 +30,36 @@ public class Bank {
     }
 
     public void withdrawFromAccount(String accountNumber, float amount) {
-        getAccount(accountNumber).withdraw(amount);
+    	Account account = getAccount(accountNumber);
+    	
+    	if (account.getAccountType().toLowerCase().equals("savings"))
+    	{
+    	    if (account.getBalance() >= 0)
+    	        account.withdraw(amount);
+    	}
+    	else
+    	{
+    		float aux = account.getBalance() - amount;
+    		
+    		if (aux >= OVERDRAFT_LIMIT)
+    	        account.withdraw(amount);
+    	}
     }
 
     public int numAccounts() {
         return accountMap.size();
+    }
+    
+    public float getCurrentHoldings()
+    {
+    	float sumOfHoldings = 0;
+
+    	for( Map.Entry<String, Account> entries : accountMap.entrySet())
+    	{
+    		Account account = entries.getValue();
+    		sumOfHoldings += account.getBalance();
+    	}
+    	
+    	return sumOfHoldings;
     }
 }
